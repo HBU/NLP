7e5547254f003f1b9640cde7cb026128ee6bc19f
David <cmc_weiyg@163.com>
2020-06-17 22:32:37 +0800

    nlp

---
+++ HanLP_Learning/book/ch02/E-2.2.2-utility.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-# Author：hankcs# Date: 2018-05-24 22:11
+# 《自然语言处理入门》2.2.2 词典的加载
+from pyhanlp import *
+
+def load_dictionary():
+    """
+    加载HanLP中的mini词库
+    :return: 一个set形式的词库
+    """
+    IOUtil = JClass('com.hankcs.hanlp.corpus.io.IOUtil')
+    path = HanLP.Config.CoreDictionaryPath.replace('.txt', '.mini.txt')
+    dic = IOUtil.loadDictionary([path])
+    return set(dic.keySet())
+
+
+if __name__ == '__main__':
+    dic = load_dictionary()
+    print(len(dic))
+    print(list(dic)[0])
